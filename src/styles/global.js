52764aa0b0de1fb351356d0348c90c7b3d325351
Juan Maxwell Noronha <juanmnoronha@gmail.com>
2019-07-21 10:14:34 -0300

    Adds styled-components and global styles setup

---
+++ src/styles/global.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    outline: 0;
+    padding: 0;
+  }
+
+  html, body, #root {
+    min-height: 100%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body, input, button {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 14px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
